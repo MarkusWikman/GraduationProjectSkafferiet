2dc8c6c658c534d44921c92a1f63a9b78b28f26d
Sebastian Fredriksson <sebastian.fredriksson1996@gmail.com>
2023-03-28 15:11:12 +0200

    Hårdkodad lösning på view och utkommenterat försök till api-call

---
+++ GraduationProjectSkafferiet/Models/RecipeInfoDto.cs
@@ -0,0 +1,21 @@
+﻿namespace GraduationProjectSkafferiet.Models
+{
+    public class RecipeInfoDto
+    {
+
+        // TODO: Ett tappert försök till att hämta api
+        public string Name { get; set; }
+        public List<StepsDto> Steps{ get; set; }
+        public class StepsDto
+        {
+            public List<IngredientDto> Ingredients{ get; set; }
+            public string Step { get; set; }
+
+            public class IngredientDto
+            {
+                public string Name { get; set; }
+            }
+        }
+
+    }
+}
---
+++ GraduationProjectSkafferiet/Views/Skafferiet/RecipeInfoVM.cs
@@ -0,0 +1,11 @@
+﻿namespace GraduationProjectSkafferiet.Views.Skafferiet
+{
+    public class RecipeInfoVM
+    {
+        public string Title { get; set; }
+        public List<string> Ingredients{ get; set; } = new List<string>();
+        public List<string> Instructions { get; set; } = new List<string>();
+
+        public string Image{ get; set; }
+    }
+}
